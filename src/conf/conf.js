c872a9f977dd779f7310ea349617f48ddfde5054
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-12-20 23:13:11 +0530

    appwrite service setup done

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    apwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    apwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    apwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    apwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    apwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 export default conf;
